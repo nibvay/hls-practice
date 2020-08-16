de21634751a6354747b8c6fea37acaa5eae93c15
Ivy Fang <ivyfang@linkernetworks.com>
2020-08-16 23:44:14 +0800

    Add server to serve client index

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const HLSServer = require('hls-server')
 const http = require('http')
+const fs = require('fs');
 
-const server = http.createServer()
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  const html = fs.readFileSync('../client/index.html', 'utf8');
+  res.write(html);
+  res.end();
+});
 
 const hls = new HLSServer(server, {
   path: '/streams',     // Base URI to output HLS streams
